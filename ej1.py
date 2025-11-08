aa77a3c9c57ff6ffe9fca9b603eaa8d8e2caf657
malustewart <mastewart@itba.edu.ar>
2025-11-08 12:15:39 -0300

    SE comparison with golden value

--- ej1.py
+++ ej1.py
@@ -80,6 +80,9 @@ def calc_predictors(h:np.ndarray, w:np.ndarray):
 
     return w0, w1, RSS, TSS, σ_sqr, SE_sqr_w0, SE_sqr_w1
 
+def is_inside_confidence_interval(value, mean, SE):
+    return value > mean - 2*SE and value < mean + 2*SE
+
 if __name__ == '__main__':
     height, weight = read_data('AlturaPeso.dat')
     N = len(height)
@@ -89,6 +92,9 @@ if __name__ == '__main__':
     W0_stds = np.zeros(len(ns))
     W1_means = np.zeros(len(ns))
     W1_stds = np.zeros(len(ns))
+
+    w0_golden, w1_golden, *_ = calc_predictors(height, weight)
+    print(f"w0 golden = {w0_golden}\nw1 golden = {w1_golden}")
 
     for i, n in enumerate(ns):
         reps = reps if n < N else 1
@@ -111,24 +117,32 @@ if __name__ == '__main__':
             σ_sqr[j] = sigma_sqr
             SE_sqr_w0[j] = se_sqr_w0
             SE_sqr_w1[j] = se_sqr_w1
+            
+        N_w0_intervals_contain_golden = sum(1 for w, SE in zip(W0, np.sqrt(SE_sqr_w0)) if is_inside_confidence_interval(w0_golden, w, SE))
+        N_w1_intervals_contain_golden = sum(1 for w, SE in zip(W1, np.sqrt(SE_sqr_w1)) if is_inside_confidence_interval(w1_golden, w, SE))
 
         w0_mean = W0.mean()
         w1_mean = W1.mean()
         w0_std = W0.std()
         w1_std = W1.std()
+        SE0_mean = np.sqrt(SE_sqr_w0).mean()
+        SE1_mean = np.sqrt(SE_sqr_w1).mean()
+        SE0_std = np.sqrt(SE_sqr_w0).std()
+        SE1_std = np.sqrt(SE_sqr_w1).std()
+
         print(f"n={n}: w0 = {w0_mean}+-{w0_std}")
         print(f"n={n}: w1 = {w1_mean}+-{w1_std}")
+        print(f"n={n}: SE0 = {SE0_mean}+-{SE0_std}")
+        print(f"n={n}: SE1 = {SE1_mean}+-{SE1_std}")
+        print(f"n={n}: N w0 intervals contains golden: {N_w0_intervals_contain_golden}/{reps} = {N_w0_intervals_contain_golden/reps}")
+        print(f"n={n}: N w1 intervals contains golden: {N_w1_intervals_contain_golden}/{reps} = {N_w1_intervals_contain_golden/reps}")
+        
         W0_means[i] = w0_mean
         W0_stds[i] = w0_std
         W1_means[i] = w1_mean
         W1_stds[i] = w1_std
 
-
-
-        plot_w_histogram_2d(W0, W1, {"n":n, "N_rep": reps})
-        plot_w_histogram_1d(W0, W1, {"n":n, "N_rep": reps})
-
-
-
+        # plot_w_histogram_2d(W0, W1, {"n":n, "N_rep": reps})
+        # plot_w_histogram_1d(W0, W1, {"n":n, "N_rep": reps})
 
     plt.show()
